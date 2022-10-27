44aca32a18e98b8053ce5ae7c308d9221c340136
Ragini340 <ragini31bxr@gmail.com>
2022-10-27 17:02:22 +0530

    Add project files.

---
+++ CRUDOperation/Models/ItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDOperation.Models
+{
+    public interface ItemRepository
+    {
+        IEnumerable<Item> GetAll();
+        Item Get(int id);
+        Item Add(Item item);
+        void Remove(int id);
+        bool Update(Item item);
+    }
+}
